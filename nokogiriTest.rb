4d804f00fa9305ef7468219c88ccdc319264edc3
justin skord <jskord@gmail.com>
2016-12-26 20:20:02 -0600

    formulating a plan

--- nokogiriTest.rb
+++ nokogiriTest.rb
@@ -3,11 +3,34 @@ require 'open-uri'
 
 url = 'http://www.ndhealth.gov/ehs/foia/spills/'
 
-keys = Nokogiri::HTML(open(url))
+raw_data = Nokogiri::HTML(open(url))
 
-keys.xpath("//tr//th").each do |key|
-  puts key.text
+# creating an empty array and an empty hash so that i can build my array of hashes!!
+# spills will be an array of arraiys with hashes in them
+# spill will be a singile spill of hashes
+# data is the spill info that will be fed into the spill array
+spills = []
+spill = []
+data = {}
+
+raw_data.xpath("//tr//th").each do |key|
+  data_keys << key.text
 end
+
+p data_keys
+
+raw_data.xpath('//td').each do |data|
+  p data.text
+end
+
+
+
+
+
+
+
+
+
 
 
 
